dfebda3adadc4d02875b54d151a5769568924019
Eddie <eddie@edwinfairchild.com>
2024-01-10 16:03:45 -0600

    clean up

--- tinyTask_STM32/tinyTasks_F4/build/tinyKernel.d
+++
@@ -1,7 +0,0 @@
-build/tinyKernel.o: tinyTasks/src/tinyKernel.c tinyTasks/inc/tinyKernel.h \
- tinyTasks/inc/tinyTasksError.h tinyTasks/inc/tinyTasksError.h \
- tinyTasks/inc/tinyTasksConfig.h
-tinyTasks/inc/tinyKernel.h:
-tinyTasks/inc/tinyTasksError.h:
-tinyTasks/inc/tinyTasksError.h:
-tinyTasks/inc/tinyTasksConfig.h:
