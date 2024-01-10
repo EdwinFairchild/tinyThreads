19ab471d2124ad8c4a4a27f7740635104855ef31
Eddie <eddie@edwinfairchild.com>
2024-01-10 15:27:16 -0600

    stm32f4 initial

---
+++ tinyTask_STM32/tinyTasks_F4/tinyTasks/inc/tinyKernel.h
@@ -0,0 +1,16 @@
+#ifndef TINYKERNEL_H
+#define TINYKERNEL_H
+
+#include "stdbool.h"
+#include "stdio.h"
+#include "stdint.h"
+#include "stdarg.h"
+#include "string.h"
+
+#include "tinyTasksError.h"
+
+TinyTasksStatus tinyKernel_init(void);
+TinyTasksStatus tinyKernel_run(void);
+TinyTasksStatus tinyKetnell_addTask(void (*task)(void), uint32_t period);
+
+#endif // TINYKERNEL_H
---
+++ tinyTask_STM32/tinyTasks_F4/tinyTasks/inc/tinyTasksError.h
@@ -0,0 +1,16 @@
+#ifndef TINYTASKSERROR_H
+#define TINYTASKSERROR_H
+
+ typedef enum TinyTasksStatus {
+    TINYTASKS_OK = 0,           // No error
+    TINYTASKS_INVALID_TASK     = -1,     // Invalid task
+    TINYTASKS_INVALID_PRIORITY = -2, // Invalid priority
+    TINYTASKS_OUT_OF_MEMORY    = -3,    // Out of memory
+    TINYTASKS_TIMEOUT = -4,          // Timeout
+    TINYTASKS_ERROR   = -5             // Misc error
+    // Add more error codes as needed
+}TinyTasksStatus;
+
+//typedef enum TinyTasksStatus TinyTasksStatus;
+
+#endif // TINYTASKSERROR_H
