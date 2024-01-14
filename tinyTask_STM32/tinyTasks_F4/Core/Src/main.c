570e1cffb4f7729e157293edd09644ad4a4da799
Eddie <eddie@edwinfairchild.com>
2024-01-13 20:02:49 -0600

    add vs code launch and tasks

--- tinyTask_STM32/tinyTasks_F4/Core/Src/main.c
+++ tinyTask_STM32/tinyTasks_F4/Core/Src/main.c
@@ -50,6 +50,7 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
+  static uint32_t count = 0;
 
 /* USER CODE END PV */
 
@@ -117,7 +118,7 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  uint32_t count = 0;
+
   while (1)
   {
     HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
