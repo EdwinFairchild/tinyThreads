ad1e45b01ab8c877d72d4cb56b7fb31b43d38753
Edwin Amaya <deusxmachina@mac.attlocal.net>
2024-01-14 10:46:30 -0600

    minor work

--- tinyTask_STM32/tinyTasks_F4/tinyTasks/inc/tinyTasksError.h
+++ tinyTask_STM32/tinyTasks_F4/tinyTasks/inc/tinyTasksError.h
@@ -7,7 +7,8 @@
     TINYTASKS_INVALID_PRIORITY = -2, // Invalid priority
     TINYTASKS_OUT_OF_MEMORY    = -3,    // Out of memory
     TINYTASKS_TIMEOUT = -4,          // Timeout
-    TINYTASKS_ERROR   = -5             // Misc error
+    TINYTASKS_MAX_TASKS_REACHED = -5, // Max tasks reached
+    TINYTASKS_ERROR   = -6             // Misc error
     // Add more error codes as needed
 }TinyTasksStatus;
 
--- tinyTask_STM32/tinyTasks_F4/tinyTasks/src/tinyKernel.c
+++ tinyTask_STM32/tinyTasks_F4/tinyTasks/src/tinyKernel.c
@@ -15,11 +15,14 @@ typedef struct tinyTask_tcb  tinyTask_tcb_t;
 typedef struct tinyTask_tcb{
     uint32_t *stackPointer; // Pointer to the current stack pointer
     tinyTask_tcb_t *next;   // Pointer to the next task
+    uint32_t period;        // Period of the task
     
 }tinyTask_tcb;
 
-tinyTask_tcb_t taskControlBlocks[TINYTASKS_MAX_TASKS];
-
+/* Task Control Block Array */
+tinyTask_tcb_t tinyTask_task_ctl[TINYTASKS_MAX_TASKS];
+typedef uint32_t tinyTask_tcb_idx;
+tinyTask_tcb_idx currentInitTask = 0;
 
 
 TinyTasksStatus tinyKernel_init(void)
@@ -36,12 +39,26 @@ TinyTasksStatus tinyKernel_init(void)
 ***************************************************************************/
 TinyTasksStatus tinyKernel_task_stack_init(uint32_t taskIDX){
     TinyTasksStatus err = TINYTASKS_OK;
+    // TODO : make macro for top of stack is 512-1
+    tinyTask_stack[taskIDX][TINYTASKS_STACK_SIZE - 1] |= (1 << 24); // xPSR
 
-    tinyTask_stack[taskIDX][TINYTASKS_STACK_SIZE - 1] |= (1 << 24);
+    // stack pointer will get initialized elsewhere 
+    // tinyTask_stack[taskIDX][TINYTASKS_STACK_SIZE - 2] = (uint32_t)taskControlBlocks[taskIDX].stackPointer;
+    tinyTask_stack[taskIDX][TINYTASKS_STACK_SIZE - 3] = 0x12345678; // R14 (LR)
+    tinyTask_stack[taskIDX][TINYTASKS_STACK_SIZE - 4] = 0x12345678; // R12 
+    tinyTask_stack[taskIDX][TINYTASKS_STACK_SIZE - 5] = 0x12345678; // R3 
+    tinyTask_stack[taskIDX][TINYTASKS_STACK_SIZE - 6] = 0x12345678; // R2
+    tinyTask_stack[taskIDX][TINYTASKS_STACK_SIZE - 7] = 0x12345678; // R1
+    tinyTask_stack[taskIDX][TINYTASKS_STACK_SIZE - 8] = 0x12345678; // R0
 
-    // init stack for given task
-    taskControlBlocks[taskIDX].stackPointer = &tinyTask_stack[taskIDX][TINYTASKS_STACK_SIZE - 1];
-    
+    tinyTask_stack[taskIDX][TINYTASKS_STACK_SIZE - 9] = 0x12345678; // R11
+    tinyTask_stack[taskIDX][TINYTASKS_STACK_SIZE - 10] = 0x12345678; // R10
+    tinyTask_stack[taskIDX][TINYTASKS_STACK_SIZE - 11] = 0x12345678; // R9
+    tinyTask_stack[taskIDX][TINYTASKS_STACK_SIZE - 12] = 0x12345678; // R8
+    tinyTask_stack[taskIDX][TINYTASKS_STACK_SIZE - 13] = 0x12345678; // R7
+    tinyTask_stack[taskIDX][TINYTASKS_STACK_SIZE - 14] = 0x12345678; // R6
+    tinyTask_stack[taskIDX][TINYTASKS_STACK_SIZE - 15] = 0x12345678; // R5
+    tinyTask_stack[taskIDX][TINYTASKS_STACK_SIZE - 16] = 0x12345678; // R4
     
     return err;
 }
@@ -54,7 +71,25 @@ TinyTasksStatus tinyKernel_run(void){
 
 TinyTasksStatus tinyKetnell_addTask(void (*task)(void), uint32_t period){
     TinyTasksStatus err = TINYTASKS_OK;
-    
+    /* disable interrupts */
+    __disable_irq();
+    if(currentInitTask <= (TINYTASKS_MAX_TASKS -1)){
+        // todo make this into a proper linked list with linked list functions for adding tasks
+        tinyTask_task_ctl[currentInitTask].next = &tinyTask_task_ctl[currentInitTask + 1];
+        //since this is a round robin scheduler this task will run for the period and then the next task will run
+        tinyTask_task_ctl[currentInitTask].period = period;
+        tinyKernel_task_stack_init(currentInitTask);
+        // initialize PC , initial program counter just points to the task
+        // during context switch we will push the PC to the stack
+        // and pop it off when we want to return to the task at its proper place
+        tinyTask_stack[currentInitTask][TINYTASKS_STACK_SIZE - 2] = (uint32_t)task;
+
+
+        currentInitTask++;
+    }
+    else{
+        err = TINYTASKS_MAX_TASKS_REACHED;
+    }
     return err;
 }
 
